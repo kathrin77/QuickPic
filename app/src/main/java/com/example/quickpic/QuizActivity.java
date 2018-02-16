68375b4a403625cb4f009a6654ddb7984a8b03b2
tn86 <tani5911@th-wildau.de>
2018-02-16 11:31:37 +0100

    Timer implementiert

--- app/src/main/java/com/example/quickpic/QuizActivity.java
+++ app/src/main/java/com/example/quickpic/QuizActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,6 +26,10 @@ public class QuizActivity extends AppCompatActivity {
 
     // Handler to delay the next question, to have time for changing the button color (correct: green, incorrect: red)
     Handler doItLater = new Handler();
+    // Handler for timeout
+    Handler timeoutHandler = new Handler();
+    // seconds till timeout
+    int timeout;
 
     //für die Leben: ein Versuch, besser wohl mit bitmaps wie in Tutorial von Nora.
     //float measure = getResources().getDisplayMetrics().density;
@@ -47,6 +52,8 @@ public class QuizActivity extends AppCompatActivity {
         tvTime = (TextView) findViewById(R.id.tvTime);
 
         imgQuiz = (ImageView) findViewById(R.id.imgQuiz);
+
+        timeout = 10500;
 
         btn1 = (Button) findViewById(R.id.btn1);
         btn2 = (Button) findViewById(R.id.btn2);
@@ -88,7 +95,7 @@ public class QuizActivity extends AppCompatActivity {
      * starts a new game, based on the topic passed on by the intent from startactivity
      */
     public void loadGame() {
-
+        timeoutHandler.removeCallbacksAndMessages(null);
         //get the selected topic from StartActivity and set game.topic:
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -122,11 +129,11 @@ public class QuizActivity extends AppCompatActivity {
      * starts a new round:
      * - sets Round to textview
      * - gets a random picture, sets correct answer to a random button, sets 3 wrong answers to the other buttons
+     * - sets Timeout, with each new round 500 ms less
      *
      * @param round
      */
     public void startNewRound(int round) {
-
         game.setRound(round);
         tvRound.setText("Round: " + game.getRound());
         //Leben werden bei jedem Rundenstart neu auf tvLives gesetzt.
@@ -180,6 +187,9 @@ public class QuizActivity extends AppCompatActivity {
                 break;
         }
 
+        timeout -= 500;
+        resetTimeout();
+
     }
 
     /**
@@ -209,12 +219,30 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     /**
+     * This method calls the GameOverActivity after a certain amount if time (int timeout)
+     * and displays the current amount of ms (int timeout)
+     */
+    public void resetTimeout() {
+        timeoutHandler.removeCallbacksAndMessages(null);
+        tvTime.setText(timeout+"");
+        timeoutHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                checkGameOver(0);
+            }
+        }, timeout);
+    }
+
+    /**
      * This method evaluates the answers and continues the game to the next round/level or ends the game.
      *
      * @param view
      */
 
     public void btnAnswerPressed(View view) {
+
+        timeoutHandler.removeCallbacksAndMessages(null);
+
         //set view to the clicked button
         final Button clicked = (Button) view;
         //get the string from the clicked button
@@ -283,6 +311,7 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     public void checkGameOver(int lives) {
+        timeoutHandler.removeCallbacksAndMessages(null);
         if (lives < 1) {
             Intent gameover = new Intent(this, GameOverActivity.class);
             startActivity(gameover);
